aef9b91bc9372bdc76f5a2792ce63b9e34e38a09
John Shen <johnzhihaoshen@gmail.com>
2017-06-29 18:41:05 +0200

    added game tick logic

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ window.onload = function(){
     document.addEventListener("keydown", keyPush);
     var py, py, vx, vy, xmax, ymax;
     initGame()
+    var framerate = 1000/15 // 66.67 fps
+    setInterval(tick, framerate)
 }
 
 function initGame() {
@@ -16,28 +18,46 @@ function initGame() {
 }
 
 function keyPush(event) {
-//Keyboard codes
-// w == 87
-// a == 65
-// s == 83
-// d == 68
+    //Keyboard codes
+    // w == 87
+    // a == 65
+    // s == 83
+    // d == 68
     switch(event.keyCode) {
         case 87:
-            console.log(`vx: ${vx}, vy: ${vy}`)
+            console.log(`px: ${px}, py: ${py}`)
             vx = 0; vy = 1;
             break;
         case 65:
-            console.log(`vx: ${vx}, vy: ${vy}`)
+            console.log(`px: ${px}, py: ${py}`)
             vx = -1; vy = 0;
             break;
         case 83:
-            console.log(`vx: ${vx}, vy: ${vy}`)
+            console.log(`px: ${px}, py: ${py}`)
             vx = 0; vy = -1;
             break;
         case 68:
-            console.log(`vx: ${vx}, vy: ${vy}`)
+            console.log(`px: ${px}, py: ${py}`)
             vx = 1; vy = 0;
             break;
     }
 }
+
+function tick() {
+   px = px += vx;
+   py = py += vy;
+   if (px > xmax) {
+        px = 0;
+   }
+   if (py > ymax) {
+        py = 0;
+   }
+   if (px < 0) {
+        px = xmax;
+   }
+   if (py < 0) {
+        py = ymax;
+   }
+}
+
 
