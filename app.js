b9b711ed4de591ddd39023a06985d79f6927089a
John Shen <johnzhihaoshen@gmail.com>
2017-06-29 18:35:12 +0200

    moved logic to app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+
+function keyPush(event) {
+    console.log(event)
+        //Keyboard codes
+        // w == 87
+        // a == 65
+        // s == 83
+        // d == 68
+            // switch(event.keyCode) {
+            //     case 87:
+            //         console.log(`vx: ${vx}, vy: ${vy}`)
+            //         vx = 0; vy = 1;
+            //         break;
+            //     case 65:
+            //         console.log(`vx: ${vx}, vy: ${vy}`)
+            //         vx = -1; vy = 0;
+            //         break;
+            //     case 83:
+            //         console.log(`vx: ${vx}, vy: ${vy}`)
+            //         vx = 0; vy = -1;
+            //         break;
+            //     case 68:
+            //         console.log(`vx: ${vx}, vy: ${vy}`)
+            //         vx = 1; vy = 0;
+            //         break;
+            // }
+}
