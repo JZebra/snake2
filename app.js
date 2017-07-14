7398cb9053a7fd497401677d8ac4e02a984e3853
John Shen <johnzhihaoshen@gmail.com>
2017-07-14 11:26:25 +0100

    render snake and game border

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ window.onload = function(){
     var canv = document.getElementById("gc");
     var ctx = canv.getContext("2d");
     document.addEventListener("keydown", keyPush);
-    var py, py, vx, vy, xmax, ymax;
-    initGame()
-    var framerate = 1000/15 // 66.67 fps
-    setInterval(tick, framerate)
+    initGame(ctx, canv)
+    var framerate = 1000/60 // 66.67 fps
+    setInterval(function() {tick(ctx, canv)}, framerate)
 }
 
-function initGame() {
+function initGame(ctx, canv) {
     px = 10;
     py = 10;
     vx = 0;
     vy = 0;
-    xmax = 20;
-    ymax = 20;
+    xmax = canv.width;
+    ymax = canv.height;
+
 }
 
 function keyPush(event) {
@@ -24,40 +24,43 @@ function keyPush(event) {
     // s == 83
     // d == 68
     switch(event.keyCode) {
-        case 87:
-            console.log(`px: ${px}, py: ${py}`)
+        case 83:
             vx = 0; vy = 1;
             break;
         case 65:
-            console.log(`px: ${px}, py: ${py}`)
             vx = -1; vy = 0;
             break;
-        case 83:
-            console.log(`px: ${px}, py: ${py}`)
+        case 87:
             vx = 0; vy = -1;
             break;
         case 68:
-            console.log(`px: ${px}, py: ${py}`)
             vx = 1; vy = 0;
             break;
     }
 }
 
-function tick() {
-   px = px += vx;
-   py = py += vy;
-   if (px > xmax) {
+function tick(ctx, canv) {
+    console.log(`px: ${px}, py: ${py}`)
+    px = px += vx;
+    py = py += vy;
+    if (px > xmax) {
         px = 0;
-   }
-   if (py > ymax) {
+    }
+    if (py > ymax) {
         py = 0;
-   }
-   if (px < 0) {
+    }
+    if (px < 0) {
         px = xmax;
-   }
-   if (py < 0) {
+    }
+    if (py < 0) {
         py = ymax;
-   }
+    }
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canv.width, canv.height)
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(px, py, 10, 10)
 }
 
 
