4807ebee98c6a7e11e6932e21d3bebded11dbc8f
John Shen <johnzhihaoshen@gmail.com>
2017-07-31 12:12:04 -0700

    refactored snake into separate class

--- app.js
+++ app.js
@@ -7,14 +7,40 @@ window.onload = function(){
     setInterval(function() {tick(ctx, canv)}, framerate)
 }
 
-function initGame(ctx, canv) {
-    snake = {
-        'px': 10,
-        'py': 10,
-        'vx': 0,
-        'vy': 0
+function Snake(px, py, vx, vy, stepsize) {
+    this.vx = vx;
+    this.vy = vy;
+    this.head = {
+        'px': px,
+        'py': py
+    };
+    this.stepsize = stepsize;
+    this.segments = [this.head];
+    this.updatePosition = function() {
+        for (var i = 0; i < this.segments.length; i++) {
+            var segment = this.segments[i];
+            segment.px = segment.px += this.vx * this.stepsize;
+            segment.py = segment.py += this.vy * this.stepsize;
+            if (segment.px > xmax) {
+                segment.px = 0;
+            }
+            if (segment.py > ymax) {
+                segment.py = 0;
+            }
+            if (segment.px < 0) {
+                segment.px = xmax;
+            }
+            if (segment.py < 0) {
+                segment.py = ymax;
+            }
+
+        }
     }
+}
+
+function initGame(ctx, canv) {
     stepsize = 10;
+    snake = new Snake(10, 10, 0, 0, stepsize)
     xmax = canv.width - stepsize;
     ymax = canv.height - stepsize;
     food = null;
@@ -28,23 +54,23 @@ function keyPush(event) {
     // d == 68
     switch(event.keyCode) {
         case 83:
-            if (snake['vy'] != -1) {
-                snake['vx'] = 0; snake['vy'] = 1;
+            if (snake.vy != -1) {
+                snake.vx = 0; snake.vy = 1;
             }
             break;
         case 65:
-            if (snake['vx'] != 1) {
-                snake['vx'] = -1; snake['vy'] = 0;
+            if (snake.vx != 1) {
+                snake.vx = -1; snake.vy = 0;
             }
             break;
         case 87:
-            if (snake['vy'] != 1) {
-                snake['vx'] = 0; snake['vy'] = -1;
+            if (snake.vy != 1) {
+                snake.vx = 0; snake.vy = -1;
             }
             break;
         case 68:
-            if (snake['vx'] != -1) {
-                snake['vx'] = 1; snake['vy'] = 0;
+            if (snake.vx != -1) {
+                snake.vx = 1; snake.vy = 0;
             }
             break;
     }
@@ -53,7 +79,7 @@ function keyPush(event) {
 function tick(ctx, canv) {
     // update positions
     makeFood(ctx, canv)
-    updateSnake(ctx, canv)
+    snake.updatePosition()
 
     // draw board
     ctx.fillStyle = "black";
@@ -62,29 +88,15 @@ function tick(ctx, canv) {
     // draw snake
     const snakesize = 10;
     ctx.fillStyle = "white";
-    ctx.fillRect(snake['px'], snake['py'], snakesize, snakesize)
+    for (var i = 0; i < snake.segments.length; i++) {
+        var segment = snake.segments[i];
+        ctx.fillRect(segment['px'], segment['py'], snakesize, snakesize)
+    }
 
     // draw food
     const foodsize = 10;
     ctx.fillStyle = "red";
     ctx.fillRect(food['x'], food['y'], foodsize, foodsize)
-}
-
-function updateSnake(ctx, canv) {
-    snake['px'] = snake['px'] += snake['vx'] * stepsize;
-    snake['py'] = snake['py'] += snake['vy'] * stepsize;
-    if (snake['px'] > xmax) {
-        snake['px'] = 0;
-    }
-    if (snake['py'] > ymax) {
-        snake['py'] = 0;
-    }
-    if (snake['px'] < 0) {
-        snake['px'] = xmax;
-    }
-    if (snake['py'] < 0) {
-        snake['py'] = ymax;
-    }
 }
 
 function makeFood(ctx, canv) {
@@ -98,8 +110,4 @@ function makeFood(ctx, canv) {
         }
     }
 }
-
-
-
-
 
